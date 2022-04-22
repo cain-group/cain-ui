3f2a93a85b24851b95d928005f1e3aa3cae2c7fa
tenbamboo <liu19891014@163.com>
2022-04-22 09:34:26 +0800

    调整路径结构

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'dumi';
-import headScripts from './um-docs/build/headScripts.js'
-import componentMenus from './um-docs/menus.js'
+import headScripts from './docs/build/headScripts.js'
+import componentMenus from './docs/menus.js'
 
 const repo = 'cain-ui';
 
@@ -12,7 +12,7 @@ export default defineConfig({
     'https://raw.githubusercontent.com/cain-group/cain-ui/main/assets/logo.png',
   logo:
     'https://raw.githubusercontent.com/cain-group/cain-ui/main/assets/logo.png',
-  outputPath: 'docs',
+  outputPath: 'docs-dist',
   mode: 'site',
   hash: true,
   // Because of using GitHub Pages
@@ -22,7 +22,7 @@ export default defineConfig({
     { from: '/www/build', to: 'build' },
   ],
   resolve:{
-    includes:['um-docs']
+    includes:['docs']
   },
   headScripts,
   menus: {
